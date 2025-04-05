1b1a23232cd7023c2c101a8829de9248c614eaaf
INN <ivngigiy@gmail.com>
2025-04-05 17:24:39 +0300

    added template and static files

--- symptom_tracker/symptom_api/settings.py
+++ symptom_tracker/symptom_api/settings.py
@@ -75,7 +75,7 @@ ROOT_URLCONF = 'symptom_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 
 # Default primary key field type
